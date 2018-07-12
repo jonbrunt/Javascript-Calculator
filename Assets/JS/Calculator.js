7c1a901bca50a5834d608dec20bbf19723bc5500
Jonathan Matthew Brunt <jbrunt.mobile@gmail.com>
2018-07-12 15:36:39 -0500

    REMOVE SOUND/PRETTIFY JS
    - in Calculator.js:
    - comment out sound function and sound invocation (line 17 and 34-37)
    - done to remove sound from app

--- Assets/JS/Calculator.js
+++ Assets/JS/Calculator.js
@@ -14,32 +14,32 @@ function main() {
 	//iterates over all key targets to select key clicked
 	for (let i = 0; i <= 18; i++) {
 		keys[i].addEventListener('click', function () {
-	    	sound(); //triggers key tone mp3
-	    	//assigns variable for use in keys that do not have a value attribute
-	    	let inner = this.innerText.replace(/\s+/g,'');
-	    	//assigns variable for all other keys based on specific value attribute
-	    	let val = this.value
-	    	if (inner === 'AC') {reset();} //if AC key, resets calculator/progran
-	    	else if (inner === 'CE') {clearEntry();} //if CE key, calls clearEntry()
-	    	else if (inner === '+/-') {posNeg();} //if +/- key, calls function that controls action
-	    	else if (val === '.') {decimalPoint(val)} //if decimal key, calls function that controls action
-	    	else if (val === '0') {zero(val);} //if 0 key, calls specific function for zero
-	    	else if (val === '=') {solve();} //if = key, solves current string of operation
-	    	else if (/^[\*|\/|\-|\+]$/.test(val)) {updateOperator(val);} //if an operator key, calls operater function
-			else {updateNumber(val);} //else is number between 1-9 and updates number via updateNumber()
+			// sound(); //triggers key tone mp3
+			//assigns variable for use in keys that do not have a value attribute
+			let inner = this.innerText.replace(/\s+/g, '');
+			//assigns variable for all other keys based on specific value attribute
+			let val = this.value
+			if (inner === 'AC') { reset(); } //if AC key, resets calculator/progran
+			else if (inner === 'CE') { clearEntry(); } //if CE key, calls clearEntry()
+			else if (inner === '+/-') { posNeg(); } //if +/- key, calls function that controls action
+			else if (val === '.') { decimalPoint(val) } //if decimal key, calls function that controls action
+			else if (val === '0') { zero(val); } //if 0 key, calls specific function for zero
+			else if (val === '=') { solve(); } //if = key, solves current string of operation
+			else if (/^[\*|\/|\-|\+]$/.test(val)) { updateOperator(val); } //if an operator key, calls operater function
+			else { updateNumber(val); } //else is number between 1-9 and updates number via updateNumber()
 		});
 	}
 }
 //invokes key click mp3
-function sound() {
-	let audio = new Audio('Assets/Sounds/multimedia_button_click_010.mp3');
-	audio.play();
-}
+// function sound() {
+// 	let audio = new Audio('Assets/Sounds/multimedia_button_click_010.mp3');
+// 	audio.play();
+// }
 //function that resets calculator/program
 function reset() {
 	//sets all global variables and output displays to initial values
 	current = '';
-	displayValue = ''; 
+	displayValue = '';
 	display.innerText = '0';
 	smallDisplay.innerText = '0';
 	clearAvailable = false;
@@ -50,7 +50,7 @@ function clearEntry() {
 	if (clearAvailable === true) { //executes if use of key is allowed
 		display.innerText = '0'; //clears main display
 		let arr = current.trim().split(' '); //splits current operation string for analysis
-		if (arr.length === 1) {reset();} //invokes reset function if current operation consists of one number and no operator
+		if (arr.length === 1) { reset(); } //invokes reset function if current operation consists of one number and no operator
 		else if (/^[\*|\/|\-|\+]$/.test(arr[arr.length - 1])) { //regex detecting presence of operator in final operation entry
 			displayValue = arr[arr.length - 2]; //assigns new displayValue as last number in operation
 			current = current.trim().split(' ').slice(0, -1).join(' '); //removes operator
@@ -69,14 +69,14 @@ function updateNumber(val) {
 	//invokes if output display does not exceed max chars and if operator key not required
 	if (displayValue.length < 13 && operatorRequired === false) {
 		current += val; //concatenates current key clicked to current number data
-		if (displayValue === '') {displayValue = val;} //if initial number in entry, displays single number
-		else {displayValue += val;} //else displays multiple digit number
+		if (displayValue === '') { displayValue = val; } //if initial number in entry, displays single number
+		else { displayValue += val; } //else displays multiple digit number
 		display.innerText = displayValue; //updates main display
 	}
 	clearAvailable = true; //reactivates clear entry button
 }
 //adds operator to operation chain
-function updateOperator(val) { 
+function updateOperator(val) {
 	//invokes if a current number is in chain 
 	if (displayValue !== '') {
 		current += (' ' + val + ' '); //adds operator to chain with leading and trailing whitespace
@@ -94,7 +94,7 @@ function posNeg() {
 		if (/^[\-]/.test(displayValue)) { //detects if current number is negative
 			displayValue = displayValue.split('').slice(1).join(''); // removes "-"
 		}
-		else {displayValue = '-' + displayValue;} //else adds negative
+		else { displayValue = '-' + displayValue; } //else adds negative
 		display.innerText = displayValue; //updates display
 		//updates current operation chain
 		current = current.split(' ').slice(0, -1).join(' ') + ' ' + displayValue;
@@ -105,9 +105,9 @@ function decimalPoint(val) {
 	//invokes if decimal is currently not in entry, and if operator is not required
 	if (/^-?[0-9]+[\.][0-9]*$/.test(displayValue) === false && operatorRequired === false) {
 		//if current entry is blank, adds a zero and trailing decimal, updates string, and display
-		if (displayValue === '') {displayValue = '0.'; current += displayValue; display.innerText = displayValue;}
+		if (displayValue === '') { displayValue = '0.'; current += displayValue; display.innerText = displayValue; }
 		//else adds decimal unless entry char count is one below limit
-		else {if (displayValue.length < 12) {updateNumber(val);}}
+		else { if (displayValue.length < 12) { updateNumber(val); } }
 	}
 	clearAvailable = true; //makes CE key available
 }
@@ -116,7 +116,7 @@ function zero(val) {
 	//invokes if current number exists and operator is not required
 	if (displayValue !== '' && operatorRequired === false) {
 		//adds zero if current character limit is not exceeded
-		if (displayValue.length < 15) {updateNumber(val);}
+		if (displayValue.length < 15) { updateNumber(val); }
 	}
 }
 //solves operation string obeying order of operation
@@ -135,7 +135,7 @@ function solve() {
 		else {// else if answer exceeds limit displays error message on both displays
 			display.innerText = 'ERROR';
 			smallDisplay.innerText = 'ERROR';
-			setTimeout(function() { //resets calculator/program on 1.5 sec delay
+			setTimeout(function () { //resets calculator/program on 1.5 sec delay
 				reset();
 			}, 1500);
 		}
@@ -144,16 +144,16 @@ function solve() {
 }
 //assigns corresponding functions to char equivalent of basic operators wherein operators in string can be parsed into mathematical functionality
 const operators = {
-    '*': function(a, b) { return a * b },
-    '/': function(a, b) { return a / b },
-    '+': function(a, b) { return a + b },
-    '-': function(a, b) { return a - b }   
+	'*': function (a, b) { return a * b },
+	'/': function (a, b) { return a / b },
+	'+': function (a, b) { return a + b },
+	'-': function (a, b) { return a - b }
 };
 //assigns char equivalents of basic operators to an array for reference in loops of parser allowing parser to follow order of operation
 const ref = ['*', '/', '+', '-'];
 //accurately rounds numbers over four decimal places
 function round(num, precision) { //accepts number to be evaluated for rounding and default degree of precion (4)
-  return Number(Math.round(num + 'e' + precision) + 'e-' + precision);
+	return Number(Math.round(num + 'e' + precision) + 'e-' + precision);
 }
 //customized parsing function that transforms string of operation into a numerical expression that can be evaluated while following order of operation
 function parser(string) {
@@ -165,9 +165,9 @@ function parser(string) {
 			//evaluates first index of operators in question, or is assigned a -1 if one is not present, referencing the ref array using the iterator
 			let x = arr.indexOf(ref[i]), y = arr.indexOf(ref[i + 1]);
 			//if * or + (second round) occurs first, or other operator doesn't exist in string
-			if ((x < y && x !== -1) || (x > y && y === -1)) { 
+			if ((x < y && x !== -1) || (x > y && y === -1)) {
 				a = i; b = x - 1; c = x + 1;
-			}	else { //if / or - (second round) occurs first
+			} else { //if / or - (second round) occurs first
 				a = i + 1; b = y - 1; c = y + 1;
 			}
 			//solution of evaluated expression, rounded via calling round(), by using appropriate operator from operators object, and the indices directly before and after the detected operator
